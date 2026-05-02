f586d2140b51fbdae9b8ba7f4fc86d4802ee0fde
Sandeep Kumar <sandeepmaucps@gmail.com>
2026-05-02 17:14:30 +0530

    Bean Added

---
+++ src/main/java/car/example/App.java
@@ -0,0 +1,19 @@
+package car.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+
+        MyBean myBean = (MyBean) context.getBean("myBean");
+        myBean.setMessage("Hello Spring World");
+        System.out.println(myBean);
+
+
+    }
+}
